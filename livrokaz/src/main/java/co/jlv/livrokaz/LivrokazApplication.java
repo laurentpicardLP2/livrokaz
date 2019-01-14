50c8c7680b57998ec03e8bbe6e8f1b24b6c62755
laurent <laurent.picard.LP2@gmail.com>
2019-01-14 14:55:03 +0100

    initialisation

---
+++ livrokaz/src/main/java/co/jlv/livrokaz/LivrokazApplication.java
@@ -0,0 +1,14 @@
+package co.jlv.livrokaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrokazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrokazApplication.class, args);
+	}
+
+}
+
