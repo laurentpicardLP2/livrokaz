c806695078dda0363a2185ec9b2fab64b4f264d8
laurent <laurent.picard.LP2@gmail.com>
2019-01-17 14:12:10 +0100

    parsing - correction

--- src/main/java/co/jlv/livrokaz/LivrokazApplication.java
+++ src/main/java/co/jlv/livrokaz/LivrokazApplication.java
@@ -48,7 +48,7 @@ public class LivrokazApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception, JSONException, MalformedURLException, IOException, ClassNotFoundException, SQLException  {
 		GoogleBook gb;
 		Gendle gendle;
-		Publisher publisher;
+		Publisher publisher = null;
 		Author author;
 		List<Author> authors = new ArrayList<Author>();
 		String sDate;
@@ -63,8 +63,10 @@ public class LivrokazApplication implements CommandLineRunner {
 		double price;
 		String textSnippet;
 		String title;
-
-		String url = "https://www.googleapis.com/books/v1/volumes?q=cooking&maxResults=40&key=AIzaSyAPOsreRHHdYcdZ4pX7YNXBujTndpGJF9k";
+		String entryCat;
+		
+		entryCat = "cooking";
+		String url = "https://www.googleapis.com/books/v1/volumes?q=" + entryCat + "&maxResults=40&key=AIzaSyAPOsreRHHdYcdZ4pX7YNXBujTndpGJF9k";
 
 		String jsonText = IOUtils.toString(new URL(url), Charset.forName("UTF-8"));
 
@@ -76,206 +78,274 @@ public class LivrokazApplication implements CommandLineRunner {
 		JSONArray items = null;
 		JSONObject item = null;
 		JSONObject searchInfo = null;
-
-		try {
-			items = json.getJSONArray("items");
-			item = (JSONObject) items.get(5);
-
-		} catch(JSONException e) {  
-		}
-
-		try {
-			searchInfo = item.getJSONObject("searchInfo");
-		} catch(JSONException e) {
-		}
-
-		JSONObject saleInfo = item.getJSONObject("saleInfo");
-		JSONObject volumeInfo = item.getJSONObject("volumeInfo");
-		JSONObject imageLink = volumeInfo.getJSONObject("imageLinks");
-		JSONArray industryIdentifiers = volumeInfo.getJSONArray("industryIdentifiers");
-		JSONObject ISBN_1010 = (JSONObject) industryIdentifiers.get(0);
-
-
-/*****************************************************************
-**************************** GENDLE ******************************
-******************************************************************/
-
-		gendle=gendleRepo.findByGendle("Cuisine");
-		if(gendle==null) {
-			gendleRepo.save(new Gendle("Cuisine"));
-			gendle=gendleRepo.findByGendle("Cuisine");
-		}
-
-/*****************************************************************
-************************* PUBLISHER ******************************
-*******************************************************************/
-
-		String publisherS = "";
-
-
-		try {
-			publisherS = volumeInfo.getString("publisher");
-
-		} catch(JSONException e) {
-			publisherS = "unknown";
-
-		}finally {
-
-			publisherS = volumeInfo.getString("publisher");
-			publisher = publisherRepo.findByPublisher(publisherS);
-			if(publisher == null) {
-				publisherRepo.save(new Publisher(publisherS));
-			}
-		}
-
-/*****************************************************************
-************************* AUTHORS ********************************
-******************************************************************/
+		JSONObject volumeInfo=null;
+		JSONObject imageLink=null;
+		JSONArray industryIdentifiers=null;
+		JSONObject ISBN_1010=null;
+		JSONObject saleInfo=null;
 		
-		JSONArray authorsAr = null;
-		//authors = null;
 
-		try {
-			authorsAr = volumeInfo.getJSONArray("authors"); 
-			for (int i = 0; i < authorsAr.length(); i++) {
-
-				author = authorRepo.findByAuthor(authorsAr.getString(i));
-				if(author == null) {
-					authorRepo.save(new Author(authorsAr.getString(i)));
-				}
-				authors.add(authorRepo.findByAuthor(authorsAr.getString(i)));
+		for(int v=0; v < json.getJSONArray("items").length(); v++) {
+			try {
+				items = json.getJSONArray("items");
+				item = (JSONObject) items.get(v);
+	
+			} catch(Exception e) {  
 			}
-
-		} catch(JSONException e) {
-			authors = null;
-
-		}
-
-/*****************************************************************
-************************** TITLE *********************************
-******************************************************************/
-		title = volumeInfo.getString("title");
-
-/*****************************************************************
-*********************** PUBLISHED DATE ***************************
-******************************************************************/ 
-		sDate = "";
-
-		try {
-			sDate = volumeInfo.getString("publishedDate");
-
-		} catch (JSONException e) {
-			sDate = "unknown";
-
-		} 
-
-/*****************************************************************
-************************ ISBN_10 *********************************
-******************************************************************/
-		codeISBN = "";
-
-		try {
-			codeISBN = ISBN_1010.getString("identifier");
-			codeISBN = codeISBN.substring(codeISBN.indexOf(":")+1);
-
-
-		}catch(JSONException e) {
-			codeISBN = "unknown";
-
-		}
-
-/*****************************************************************
-************************* PAGE COUNT *****************************
-******************************************************************/
-		pageCount = 0;
-
-		try {
-			pageCount = volumeInfo.getInt("pageCount");
-
-		}catch(JSONException e) {
+	
+			try {
+				searchInfo = item.getJSONObject("searchInfo");
+			} catch(Exception e) {  
+			}
+			
+			try {
+				saleInfo = item.getJSONObject("saleInfo");
+			}catch(Exception e) {  
+			}
+			try {
+				volumeInfo = item.getJSONObject("volumeInfo");
+			}catch(JSONException e) {  
+			}
+			try {
+				imageLink = volumeInfo.getJSONObject("imageLinks");
+			}catch(JSONException e) {  
+			}
+			try {
+				industryIdentifiers = volumeInfo.getJSONArray("industryIdentifiers");
+			}catch(JSONException e) {  
+			}
+			
+			if (industryIdentifiers!=null) {
+				try {
+					ISBN_1010 = (JSONObject) industryIdentifiers.get(0);
+				}catch(JSONException e) {  
+				}
+			}
+			
+	
+	
+	
+	/*****************************************************************
+	**************************** GENDLE ******************************
+	******************************************************************/
+	
+			gendle=gendleRepo.findByGendle(entryCat);
+			if(gendle==null) {
+				gendleRepo.save(new Gendle(entryCat));
+				gendle=gendleRepo.findByGendle(entryCat);
+			}
+	
+	/*****************************************************************
+	************************* PUBLISHER ******************************
+	*******************************************************************/
+	
+			
+			String publisherS="";
+	
+			if(volumeInfo!=null) {
+					try {
+					publisherS = volumeInfo.getString("publisher");
+		
+				} catch(JSONException e) {
+					publisherS = "unknown";
+		
+				}finally {
+					
+					publisher = publisherRepo.findByPublisher(publisherS);
+					if(publisher == null) {
+						publisherRepo.save(new Publisher(publisherS));
+					}
+				}
+				
+			}else {
+				if(publisher == null) {
+					publisherRepo.save(new Publisher(publisherS));
+				}
+			}
+	
+	/*****************************************************************
+	************************* AUTHORS ********************************
+	******************************************************************/
+			
+			JSONArray authorsAr = null;
+			//authors = null;
+			
+			
+			if(volumeInfo!=null) {
+				try {
+					authorsAr = volumeInfo.getJSONArray("authors"); 
+					
+					
+						for (int i = 0; i < authorsAr.length(); i++) {
+			
+							author = authorRepo.findByAuthor(authorsAr.getString(i));
+							if(author == null) {
+								authorRepo.save(new Author(authorsAr.getString(i)));
+							}
+							try {
+								authors.add(authorRepo.findByAuthor(authorsAr.getString(i)));
+							} catch(Exception e) {
+								authors = null;
+							}
+							
+								
+						}
+					
+		
+				} catch(Exception e) {
+					authors = null;
+		
+				}
+			}else {
+				authors = null;
+			}
+				
+	
+	/*****************************************************************
+	************************** TITLE *********************************
+	******************************************************************/
+			if(volumeInfo!=null) {
+				try {
+					title = volumeInfo.getString("title");
+				} catch(Exception e) {
+					title="";
+				}
+			}else {
+				title=""; 
+			}
+	
+	/*****************************************************************
+	*********************** PUBLISHED DATE ***************************
+	******************************************************************/ 
+			sDate = "";
+			if(volumeInfo!=null) {
+				try {
+					sDate = volumeInfo.getString("publishedDate");
+		
+				} catch (JSONException e) {
+					sDate = "unknown";
+		
+				} 
+			} 
+	
+	/*****************************************************************
+	************************ ISBN_10 *********************************
+	******************************************************************/
+			codeISBN = "";
+			if(ISBN_1010!=null) {
+				try {
+					codeISBN = ISBN_1010.getString("identifier");
+					codeISBN = codeISBN.substring(codeISBN.indexOf(":")+1);
+		
+		
+				}catch(JSONException e) {
+					codeISBN = "unknown";
+		
+				}
+			}
+	
+	/*****************************************************************
+	************************* PAGE COUNT *****************************
+	******************************************************************/
 			pageCount = 0;
-
+	
+			if(volumeInfo!=null) {
+				try {
+					pageCount = volumeInfo.getInt("pageCount");
+		
+				}catch(JSONException e) {
+					pageCount = 0;
+		
+				}
+			}
+	
+	/*****************************************************************
+	************************** THUMBNAIL *****************************
+	******************************************************************/
+			imgThumbnail = "";
+	
+			try {
+				imgThumbnail = imageLink.getString("smallThumbnail");
+	
+			}catch(JSONException e) {
+				imgThumbnail = "can't find thumbnail";
+	
+			}
+	
+	
+	/*****************************************************************
+	**************************** PRICE *******************************
+	******************************************************************/
+			price = 0;
+	
+			if(saleInfo!=null) {
+				try {
+					JSONObject listPrice = saleInfo.getJSONObject("listPrice");
+					price = listPrice.getDouble("amount");
+		
+				} catch(JSONException e) {
+					price =0;
+		
+				} 
 		}
-
-/*****************************************************************
-************************** THUMBNAIL *****************************
-******************************************************************/
-		imgThumbnail = "";
-
-		try {
-			imgThumbnail = imageLink.getString("smallThumbnail");
-
-		}catch(JSONException e) {
-			imgThumbnail = "can't find thumbnail";
-
-		}
-
-
-/*****************************************************************
-**************************** PRICE *******************************
-******************************************************************/
-		price = 0;
-
-		try {
-			JSONObject listPrice = saleInfo.getJSONObject("listPrice");
-			price = listPrice.getDouble("amount");
-
-		} catch(JSONException e) {
-			price =0;
-
-		} 
-/*****************************************************************
-*************************** SNIPPET ******************************
-******************************************************************/
-		textSnippet = "";
-
-		try {
-			textSnippet = searchInfo.getString("textSnippet");
-		} catch (JSONException e) {
-
-			textSnippet = "can't find snippet";
-		}catch(NullPointerException n) {
-			textSnippet = "can't find snippet";
-		}
-
-
-
-/*****************************************************************
-************************* DESCRIPTION ****************************
-******************************************************************/
-		description = "";
-
-		try {
-			description = volumeInfo.getString("description");
-		} catch (JSONException e) {
+	/*****************************************************************
+	*************************** SNIPPET ******************************
+	******************************************************************/
+			textSnippet = "";
+	
+			if(searchInfo!=null) {
+				try {
+					textSnippet = searchInfo.getString("textSnippet");
+				} catch (JSONException e) {
+		
+					textSnippet = "can't find snippet";
+				}catch(NullPointerException n) {
+					textSnippet = "can't find snippet";
+				}
+			}
+	
+	
+	
+	/*****************************************************************
+	************************* DESCRIPTION ****************************
+	******************************************************************/
 			description = "can't find description";
-
-		} catch(NullPointerException n) {
-			description = "can't find description";
+	
+			if(volumeInfo!=null) {
+				try {
+					description = volumeInfo.getString("description");
+				} catch (JSONException e) {
+					description = "can't find description";
+		
+				} catch(NullPointerException n) {
+					description = "can't find description";
+		
+				}
+			}
+	
+	/*****************************************************************
+	*************************** eBOOK ********************************
+	******************************************************************/
+			isEbook = true;
+	
+			if(saleInfo!=null) {
+				try {
+					isEbook = saleInfo.getBoolean("isEbook");
+		
+				} catch(JSONException e) {
+					isEbook = false;
+				}
+			}
+	
+			availableQuantity = 10;
+			categorie = entryCat;
+			langage = "en";
+	
+			gb = new GoogleBook(gendle, publisher, authors, sDate, 
+					availableQuantity, categorie, codeISBN, description, imgThumbnail,
+					isEbook, langage, pageCount, price, textSnippet, title);
+			googleBookRepo.save(gb);
 
 		}
-
-/*****************************************************************
-*************************** eBOOK ********************************
-******************************************************************/
-		isEbook = true;
-
-		try {
-			isEbook = saleInfo.getBoolean("isEbook");
-
-		} catch(JSONException e) {
-			isEbook = false;
-		}
-
-		availableQuantity = 10;
-		categorie = "Cuisine";
-		langage = "fr";
-
-		gb = new GoogleBook(gendle, publisher, authors, sDate, 
-				availableQuantity, categorie, codeISBN, description, imgThumbnail,
-				isEbook, langage, pageCount, price, textSnippet, title);
-		googleBookRepo.save(gb);
-
-
 	}
 }
