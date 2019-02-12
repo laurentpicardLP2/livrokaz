e00afd6e8174818b5329a5a9eff4f786178da6ff
jprodhomme <prodhomme.julien@gmail.com>
2019-02-12 14:29:10 +0100

    BookController Update

--- src/main/java/co/jlv/livrokaz/controller/BookController.java
+++ src/main/java/co/jlv/livrokaz/controller/BookController.java
@@ -1,15 +1,14 @@
 package co.jlv.livrokaz.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.jlv.livrokaz.model.Author;
-import co.jlv.livrokaz.model.Gendle;
 import co.jlv.livrokaz.model.GoogleBook;
 import co.jlv.livrokaz.repository.AuthorRepository;
 import co.jlv.livrokaz.repository.GendleRepository;
@@ -30,6 +28,7 @@ import co.jlv.livrokaz.services.GestionCategories;
 
 @RestController
 @RequestMapping("/livrokaz")
+@CrossOrigin("http://localhost:4200")
 public class BookController {
 
 	@Autowired
